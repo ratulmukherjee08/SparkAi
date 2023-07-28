bb8e312237992c1dc79b410909fd364d536a4eea
AsifShaikh01 <asifshaikh0024@gmail.com>
2023-07-28 10:04:32 +0530

    color chnaged

--- Frontend/my-app/src/Components/InterviewPage.jsx
+++ Frontend/my-app/src/Components/InterviewPage.jsx
@@ -17,7 +17,7 @@ const InterviewPage = () => {
       </div>
 
       <div className="w-full h-140 flex justify-center items-center bg-sky-950 mt-14">
-        <input className="border-black border-1 w-2/5 p-1 m-10 rounded-5px font-bold font-mono text-black px-5" type="text" placeholder='type your answer'  />
+        <input className="border-black border-1 w-2/5 p-1 m-12 rounded-5px font-bold font-mono text-black px-5" type="text" placeholder='type your answer'  />
         <button className="bg-orange-600 w-24 text-white rounded-5px border-red-600 border-1px  p-1 hover:bg-white hover:text-black font-mono font-bold">submit</button>
       </div>
 
