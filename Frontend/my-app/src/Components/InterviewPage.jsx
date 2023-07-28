e92d37365a73959fe1455c01e6a5d53a6575da8b
AsifShaikh01 <asifshaikh0024@gmail.com>
2023-07-28 13:39:17 +0530

    single questions

--- Frontend/my-app/src/Components/InterviewPage.jsx
+++ Frontend/my-app/src/Components/InterviewPage.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import {useState , useEffect} from "react";
 
 
 const InterviewPage = () => {
+  const [questions , setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  // Function to move to the next question
+  const moveToNextQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+  };
+
+  
+
+
+  const getQuestions = async ()=>{
+    try {
+      let res = await fetch(`https://sparai-backend-app.onrender.com/questions`)
+      let data = await res.json();
+      console.log(data.msg)
+      setQuestions(data.msg)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getQuestions();
+    // Check if we have reached the end of the questions array
+    if (currentQuestionIndex < questions.length) {
+      // Set a timeout for 4 minutes before moving to the next question
+      const timeoutId = setTimeout(moveToNextQuestion, 60000);
+
+      // Cleanup the timeout when the component unmounts or the question changes
+      return () => clearTimeout(timeoutId);
+    }
+  }, [currentQuestionIndex]);
+  // useEffect(()=>{
+  //   getQuestions();
+
+  // },[])
+
+  console.log(questions)
   return (
     <>
    <div className="w-full h-14 flex justify-between items-center bg-sky-950">
@@ -12,11 +53,18 @@ const InterviewPage = () => {
         </div>
       </div>
 
-      <div className="w-full h-520 flex justify-between items-center ">
-        <textarea className="w-full border-solid border-black m-8 " rows="15"></textarea>
+      <div className="w-full p-24 m-2">
+        <div className="w-full h-full border-solid border-2 border-black m-8 p-8 font-mono text-xl font-bold " rows="15">
+        {currentQuestionIndex < questions.length ? (
+        <p>{"Q."+currentQuestionIndex+"."+questions[currentQuestionIndex].question+"?"}</p>
+      ) : (
+        <p>No more questions.</p>
+      )}
+
+        </div>
       </div>
 
-      <div className="w-full h-140 flex justify-center items-center bg-sky-950 mt-14">
+      <div className="w-full h-200 flex justify-center items-center bg-sky-950 mt-28">
         <input className="border-black border-1 w-2/5 p-1 m-12 rounded-5px font-bold font-mono text-black px-5" type="text" placeholder='type your answer'  />
         <button className="bg-orange-600 w-24 text-white rounded-5px border-red-600 border-1px  p-1 hover:bg-white hover:text-black font-mono font-bold">submit</button>
       </div>
