d27b8e93d7a166e402b1ec6d8a26c7e55ba756bb
Saman <pradhansaman2017@gmail.com>
2023-07-28 19:58:09 +0530

    be changes

--- SparkAI-Backend/index.js
+++ SparkAI-Backend/index.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-
+var cors = require("cors");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const { connection } = require("./config/db");
 const { interviewRouter } = require("./routes/interview.route");
 const { questionRouter } = require("./routes/question.route");
 const PORT = process.env.PORT;
+
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+//Swagger options declaired
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: " SparkAI_Interview API",
+      version: "1.0.0",
+      description: "API documentation for SparkAI_Interview App",
+    },
+    servers: [{ url: "http://localhost:4000/" }],
+    basePath: "/",
+  },
+  apis: ["routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //home route
 app.get("/", (req, res) => {
@@ -20,6 +42,7 @@ app.use("/questions", questionRouter);
 //intervew route
 app.use("/interview", interviewRouter);
 
+//app is listening
 app.listen(PORT, async () => {
   console.log("app is listening at ", PORT);
   try {
--- SparkAI-Backend/routes/question.route.js
+++ SparkAI-Backend/routes/question.route.js
@@ -4,6 +4,56 @@ const questionRouter = express.Router();
 const { questionModel } = require("../models/questions.model");
 
 //adding questions - POST route
+
+/**
+ * @swagger
+ * https://sparai-backend-app.onrender.com/questions/add:
+ *   post:
+ *     summary: Adding new questions
+ *     description: adding new questions to database with perticular topics via POST route
+ *     tags:
+ *       - questions
+ *     parameters:
+ *       - in: query
+ *         name: questions
+ *         description: questions data
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             topic:
+ *               type: string
+ *               description: The topic of question we want to add
+ *             question:
+ *               type: string
+ *               description: The question we want to add.
+ *         example:
+ *           topic: "NodeJS?"
+ *           question: "What is Node.js?"
+ *
+ *     responses:
+ *       200:
+ *         description: Question added to DB successfully
+ *         schema:
+ *           type: object
+ *           properties:
+ *             bot:
+ *               type: string
+ *               description: The response from the bot.
+ *           example:
+ *             bot: "The bot's response"
+ *       400:
+ *         description: Internal server error
+ *         schema:
+ *           type: object
+ *           properties:
+ *             error:
+ *               type: string
+ *               description: The error message.
+ *           example:
+ *             error: "Something went wrong"
+ */
+
 questionRouter.post("/add", async (req, res) => {
   try {
     const { topic, question } = req.body;
@@ -16,9 +66,55 @@ questionRouter.post("/add", async (req, res) => {
 });
 
 //getting questions - GET route
+/**
+ * @swagger
+ * https://sparai-backend-app.onrender.com/questions/:
+ *   get:
+ *     summary: Getting questions
+ *     description: Getting questions related to a perticular topic from database via GET route
+ *     tags:
+ *       - questions
+ *     parameters:
+ *       - in: body
+ *         name: questions
+ *         description: questions data
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             topic:
+ *               type: string
+ *               description: The topic of question we want to get
+ *
+ *         example:
+ *           topic: "NodeJS?"
+ *           question: "What is Node.js?"
+ *     responses:
+ *       200:
+ *         description: Question we got
+ *         schema:
+ *           type: object
+ *           properties:
+ *             bot:
+ *               type: string
+ *               description: The response from the bot.
+ *           example:
+ *             bot: "The bot's response"
+ *       400:
+ *         description: Internal server error
+ *         schema:
+ *           type: object
+ *           properties:
+ *             error:
+ *               type: string
+ *               description: The error message.
+ *           example:
+ *             error: "Something went wrong"
+ */
+
 questionRouter.get("/", async (req, res) => {
   try {
-    const { topic } = req.body;
+    const topic = req.query.topic;
     console.log(topic);
     if (!topic) {
       const getQuestion = await questionModel.find();
@@ -33,9 +129,52 @@ questionRouter.get("/", async (req, res) => {
 });
 
 //updating a question - PATCH route
+
+/**
+ * @swagger
+ * https://sparai-backend-app.onrender.com/questions/update/:id:
+ *   patch:
+ *     summary: Updateing a question
+ *     description: Updating a question with it's id from database via PATCH route
+ *     tags:
+ *       - questions
+ *     parameters:
+ *       - in: params
+ *         name: questions
+ *         description: question's id
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: string
+ *               description: The id of question we want to update
+ *
+ *
+ *     responses:
+ *       200:
+ *         description: Question is updated
+ *         schema:
+ *           type: object
+ *           properties:
+ *             bot:
+ *               type: string
+ *               description: The response from the bot.
+ *           example:
+ *             bot: "The bot's response"
+ *       400:
+ *         description: Internal server error
+ *         schema:
+ *           type: object
+ *           properties:
+ *             error:
+ *               type: string
+ *               description: The error message.
+ *           example:
+ *             error: "Something went wrong"
+ */
 questionRouter.patch("/update/:id", async (req, res) => {
   let id = req.params.id;
-  console.log(req.body, id);
   try {
     await questionModel.findByIdAndUpdate(id, req.body);
 
@@ -44,6 +183,51 @@ questionRouter.patch("/update/:id", async (req, res) => {
     res.status(400).send({ msg: error.message });
   }
 });
+
+//deleting a question - DELETE route
+/**
+ * @swagger
+ * https://sparai-backend-app.onrender.com/questions/delete/:id:
+ *   delete:
+ *     summary: deleteing a question
+ *     description: Updating a question with it's id from database via PATCH route
+ *     tags:
+ *       - questions
+ *     parameters:
+ *       - in: params
+ *         name: questions
+ *         description: question's id
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: string
+ *               description: The id of question we want to delete
+ *
+ *
+ *     responses:
+ *       200:
+ *         description: Question is deleted
+ *         schema:
+ *           type: object
+ *           properties:
+ *             bot:
+ *               type: string
+ *               description: The response from the bot.
+ *           example:
+ *             bot: "The bot's response"
+ *       400:
+ *         description: Internal server error
+ *         schema:
+ *           type: object
+ *           properties:
+ *             error:
+ *               type: string
+ *               description: The error message.
+ *           example:
+ *             error: "Something went wrong"
+ */
 questionRouter.delete("/delete/:id", async (req, res) => {
   let id = req.params.id;
   try {
